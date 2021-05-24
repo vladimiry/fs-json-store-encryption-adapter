4eed3300e4b58a320113b8954e55d2fe3a7df4a5
Vladimir Y <1560781+vladimiry@users.noreply.github.com>
2021-05-24 12:59:12 +0300

    make custom encryption/decryption errors more verbose

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,13 +1,20 @@
 import {BaseError} from "make-error-cause";
 
+const appendCauseMessage = (message?: string): string => {
+    const suffix = `(print "cause" prop of error to see its origin)`;
+    return message
+        ? ` ${message} ${suffix}`
+        : suffix;
+}
+
 export class EncryptionError extends BaseError {
     constructor(message: string, cause?: Error) {
-        super(message, cause);
+        super(message + appendCauseMessage(cause?.message), cause);
     }
 }
 
 export class DecryptionError extends BaseError {
     constructor(message: string, cause?: Error) {
-        super(message, cause);
+        super(message + appendCauseMessage(cause?.message), cause);
     }
 }
